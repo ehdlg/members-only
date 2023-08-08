66cfc0951bd230f3c7c3c0bc0f45acf4dbdfb7a5
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-08 11:56:45 +0200

    Project structure

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const mongoDb = 'mongodb://localhost:27017/members-only';
+
+mongoose.connect(mongoDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Succesful connection to MongoDB');
+});
