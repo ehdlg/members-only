02332485199784855fe00b77537c02b3ccea02d3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-08 12:07:26 +0200

    Changed require for mongoose, only requiring Schema

--- modules/users.js
+++ modules/users.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Schema = require('mongoose').Schema;
 
+//user schema
 const UserSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
