ac5c8ffe691d688b7e542e19d3e7d75c372c82ff
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-09 12:03:01 +0200

    Password util module for generating and validating passwords from users

---
+++ utils/passwordUtils.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt');
+
+const validPassword = (password, hash, salt) => {
+  const convertedPwd = bcrypt.hashSync(password, salt);
+  return convertedPwd === hash;
+};
+
+const generatePassword = (password) => {
+  const SALT_ROUNDS = 10; 
+  const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  return {
+    salt,
+    hashedPassword,
+  };
+};
+
+module.exports = { generatePassword, validPassword };
