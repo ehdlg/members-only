ca27f81f04463946746aaa22dbb723cbfafcce9c
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-09 12:05:02 +0200

    get and post routes for register

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
 router.get('/', (req, res, next) => {
   res.render('home', { title: 'Home' });
 });
+
+router.get('/register', userController.user_create_get);
+
+router.post('/register', userController.user_create_post);
 
 module.exports = router;
