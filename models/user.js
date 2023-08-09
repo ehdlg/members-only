1bdfeb5e089af96b4e27ea1fe5a7ae8c97cc6b99
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-09 11:04:44 +0200

    added salt field to the user for hash password

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   last_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
+  salt: { type: String, required: true },
   email: { type: String, required: true },
   membership: { type: String, default: 'Not member' },
 });
