8ff9c80ac252ae5e99626864dd2edb26cca046e3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-31 10:02:25 +0200

    Added user ref field

--- models/message.js
+++ models/message.js
@@ -5,6 +5,7 @@ const MessageSchema = new Schema({
   title: { type: String, required: true, maxLength: 300 },
   timestamp: { type: Date, default: Date.now },
   text: { type: String, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
